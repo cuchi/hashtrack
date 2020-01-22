7b273431cb89913e124885c185ee5424d7c35240
Paulo Henrique Cuchi <paulo.cuchi@gmail.com>
2020-01-21 23:11:34 -0300

    feat(infra): use heroku addons

--- infra/main.tf
+++ infra/main.tf
@@ -2,10 +2,24 @@
 provider "heroku" {}
 
 variable "heroku_app_name" {
-  description = "Name of the Heroku app provisioned as an example"
+  description = "Name of the Heroku app that will be the instance of Hashtrack"
 }
 
 resource "heroku_app" "hashtrack" {
   name   = var.heroku_app_name
   region = "us"
+}
+
+resource "heroku_addon" "database" {
+  app = heroku_app.hashtrack.name
+  plan = "heroku-postgresql:hobby-dev"
+}
+
+resource "heroku_addon" "cache" {
+  app = heroku_app.hashtrack.name
+  plan = "heroku-redis:hobby-dev"
+}
+
+output "app" {
+  value = heroku_app.hashtrack
 }
