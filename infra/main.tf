050fe17827c53d72c9a46fd3d06ac10b738f8795
Paulo Henrique Cuchi <paulo.cuchi@gmail.com>
2020-01-23 23:02:02 -0300

    chore(infra): add twitter api & consumer keys to terraform config

--- infra/main.tf
+++ infra/main.tf
@@ -5,18 +5,29 @@ variable "heroku_app_name" {
   description = "Name of the Heroku app that will be the instance of Hashtrack"
 }
 
+variable "twitter_consumer_key" {}
+variable "twitter_consumer_secret" {}
+variable "twitter_access_token" {}
+variable "twitter_access_token_secret" {}
+
 resource "heroku_app" "hashtrack" {
   name   = var.heroku_app_name
   region = "us"
+  sensitive_config_vars = {
+    TWITTER_CONSUMER_KEY        = var.twitter_consumer_key
+    TWITTER_CONSUMER_SECRET     = var.twitter_consumer_secret
+    TWITTER_ACCESS_TOKEN        = var.twitter_access_token
+    TWITTER_ACCESS_TOKEN_SECRET = var.twitter_access_token_secret
+  }
 }
 
 resource "heroku_addon" "database" {
-  app = heroku_app.hashtrack.name
+  app  = heroku_app.hashtrack.name
   plan = "heroku-postgresql:hobby-dev"
 }
 
 resource "heroku_addon" "cache" {
-  app = heroku_app.hashtrack.name
+  app  = heroku_app.hashtrack.name
   plan = "heroku-redis:hobby-dev"
 }
 
