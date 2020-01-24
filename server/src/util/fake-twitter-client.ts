f958e636f98980050339cfeb20a2bfac8a3d8629
Paulo Henrique Cuchi <paulo.cuchi@gmail.com>
2020-01-23 22:47:07 -0300

    fix(server): fix dotenv loading & some env vars

--- server/src/util/fake-twitter-client.ts
+++ server/src/util/fake-twitter-client.ts
@@ -1,12 +1,37 @@
 import { TwitterClient, Options } from '../services/twitter-client-service'
 import { format } from 'date-fns'
 import { EventEmitter } from 'typeorm/platform/PlatformTools'
+import Container from 'typedi'
+import faker from 'faker'
+import HashtagService from '../services/hashtag-service'
+import config from '../config'
 
 export class FakeTwitterClient implements TwitterClient {
     
     public currentRoute?: string
     public currentOptions?: Options
     private listener?: (...args: unknown[]) => void | Promise<void>
+
+    constructor() {
+        if (!config.twitter.streamFakeTweets) {
+            return
+        }
+
+        setInterval(async () => {
+            const hashtags = await Container.get(HashtagService).getAllActive()
+            if (!hashtags.length) {
+                return
+            }
+            const hashtag = hashtags[
+                Math.floor(Math.random() * hashtags.length)
+            ]
+            await this.sendFakeTweet(
+                faker.lorem.sentence(20), 
+                faker.internet.userName(), 
+                [hashtag.name]
+            )
+        }, 5000)
+    }
 
     stream(route: string, options: Options) {
         this.currentRoute = route
