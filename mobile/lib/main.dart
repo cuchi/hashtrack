bafc8fb963e7aea28fd837478332bf773ff92f33
Paulo Henrique Cuchi <paulo.cuchi@gmail.com>
2020-01-29 00:42:27 -0300

    refactor(mobile): add analysis file & fix lint issues

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,84 +1,84 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      theme: new ThemeData(primaryColor: Colors.blue),
-      home: new LoginPage(),
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: LoginPage(),
     );
   }
 }
 
 class LoginPage extends StatefulWidget {
   @override
-  State createState() => new LoginPageState();
+  State createState() => _LoginPageState();
 }
 
-class LoginPageState extends State<LoginPage>
+class _LoginPageState extends State<LoginPage>
     with SingleTickerProviderStateMixin {
-  Animation<double> _iconAnimation;
   AnimationController _iconAnimationController;
 
   @override
   void initState() {
     super.initState();
-    _iconAnimationController = new AnimationController(
-        vsync: this, duration: new Duration(milliseconds: 500));
-    _iconAnimation = new CurvedAnimation(
+    _iconAnimationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500));
+    CurvedAnimation(
       parent: _iconAnimationController,
       curve: Curves.bounceOut,
-    );
-    _iconAnimation.addListener(() => this.setState(() {}));
+    ).addListener(() => setState(() {}));
     _iconAnimationController.forward();
   }
 
+  TextFormField _buildInput(String label, TextInputType type,
+          {bool obscure = false}) =>
+      TextFormField(
+          decoration: InputDecoration(labelText: label),
+          keyboardType: type,
+          obscureText: obscure);
+
+  MaterialButton _buildLoginButton() => MaterialButton(
+        height: 50.0,
+        minWidth: 150.0,
+        color: Colors.green,
+        splashColor: Colors.teal,
+        textColor: Colors.white,
+        child: Text('Login'),
+        onPressed: () {},
+      );
+
+  Form _buildLoginForm() => Form(
+        autovalidate: true,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            _buildInput('Email', TextInputType.emailAddress),
+            _buildInput('Password', TextInputType.text, obscure: true),
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+            ),
+            _buildLoginButton()
+          ],
+        ),
+      );
+
   @override
   Widget build(BuildContext context) {
-    final loginButton = new MaterialButton(
-      height: 50.0,
-      minWidth: 150.0,
-      color: Colors.green,
-      splashColor: Colors.teal,
-      textColor: Colors.white,
-      child: Text('Login'),
-      onPressed: () {},
-    );
-
-    input(label, type, {obscure: false}) => TextFormField(
-          decoration: new InputDecoration(labelText: label),
-          keyboardType: type,
-          obscureText: obscure
-        );
-
-    final loginForm = new Form(
-      autovalidate: true,
-      child: new Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          input("Email", TextInputType.emailAddress),
-          input("Password", TextInputType.text, obscure: true),
-          new Padding(
-            padding: const EdgeInsets.only(top: 60.0),
-          ),
-          loginButton
-        ],
-      ),
-    );
-
-    return new Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
-      body: new Stack(fit: StackFit.expand, children: <Widget>[
-        new Column(
+      body: Stack(fit: StackFit.expand, children: <Widget>[
+        Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
               'Hashtrack App',
               style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
             ),
-            new Container(padding: const EdgeInsets.all(40.0), child: loginForm)
+            Container(
+                padding: const EdgeInsets.all(40.0), child: _buildLoginForm())
           ],
         ),
       ]),
